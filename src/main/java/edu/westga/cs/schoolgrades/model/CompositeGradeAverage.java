220caabb134861ebeffb8055067630dbcc3327e3
Ashley Metz <mrsas@192.168.0.5>
2019-10-22 16:01:23 -0600

    Created class and applyStrategy method

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/CompositeGradeAverage.java
@@ -0,0 +1,16 @@
+package edu.westga.cs.schoolgrades.model;
+
+import java.util.List;
+
+public class CompositeGradeAverage implements GradeStrategy {
+
+
+	@Override
+	public double applyStrategy(List<Grade> list) {
+		double total = 0.0;
+		for(Grade g : list) {
+			total += g.getValue();
+		}
+		return total/list.size();
+	}
+}
