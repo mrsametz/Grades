fa9a3f9d9a5f241b3aebfca880fc39f9a4e81d52
Ashley Metz <mrsas@192.168.0.5>
2019-10-28 13:58:57 -0600

    redid getAll method

--- src/main/java/edu/westga/cs/schoolgrades/model/CompositeGrade.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/CompositeGrade.java
@@ -8,30 +8,30 @@ public class CompositeGrade implements Grade {
 	private GradeStrategy strategy;
 	private List<Grade> list;
 
-	
-	public CompositeGrade() {
+	public CompositeGrade(GradeStrategy strategy) {
 		list = new ArrayList<>();
+		this.strategy = strategy;
 	}
-	
+
 	@Override
 	public double getValue() {
-		
+
 		return strategy.applyStrategy(list);
 	}
-	
+
 	public void add(Grade grade) {
 		list.add(grade);
 	}
-	
+
 	public List<Grade> getAll() {
 		return list;
 	}
-	
+
 	@Override
 	public String toString() {
 		String output = "";
-		for(Grade g : list) {
-			if(output.length() > 0) {
+		for (Grade g : list) {
+			if (output.length() > 0) {
 				output += ", ";
 			}
 			output += g.toString();
