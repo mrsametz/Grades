1d609fc76e356f90ce373b8a81a764796c23d796
Ashley Metz <mrsas@192.168.0.5>
2019-11-08 10:16:01 -0700

    Added author and version

--- src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
+++ src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
@@ -3,13 +3,19 @@ package edu.westga.cs.schoolgrades.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class SchoolGradesController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		
+		}
+	@FXML
+	public void clickReset(ActionEvent event) {
+		System.out.print("hi");
 		
 	}
 
