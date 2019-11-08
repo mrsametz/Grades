eaa184e24ecb116efcfcc8f099423b26a11a3b9e
Ashley Metz <mrsas@192.168.0.5>
2019-11-08 13:34:53 -0700

    test

--- src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
+++ src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
@@ -9,7 +9,7 @@ public class SchoolGradesController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
