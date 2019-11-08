d797e53a7a1ea4c5fbee9a99990468defd46d922
Ashley Metz <mrsas@192.168.0.5>
2019-11-08 13:26:16 -0700

    Added gui and controller

--- src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
+++ src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
@@ -1,5 +1,16 @@
 package edu.westga.cs.schoolgrades.controllers;
 
-public class SchoolGradesController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SchoolGradesController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
