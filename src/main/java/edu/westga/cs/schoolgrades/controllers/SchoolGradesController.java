e65f4f12f9d7285ef76fa5e72bfc8ad2224bc751
Ashley Metz <mrsas@192.168.0.5>
2019-11-11 17:32:03 -0700

    Added TextFieldListCell

--- src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
+++ src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
@@ -7,106 +7,58 @@ import java.util.ResourceBundle;
 import edu.westga.cs.schoolgrades.model.CompositeGrade;
 import edu.westga.cs.schoolgrades.model.CompositeGradeSum;
 import edu.westga.cs.schoolgrades.model.Grade;
-import edu.westga.cs.schoolgrades.model.GradeStrategy;
 import edu.westga.cs.schoolgrades.model.SimpleGrade;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ListView.EditEvent;
+
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.TextFieldListCell;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+
+import javafx.util.StringConverter;
 
 public class SchoolGradesController implements Initializable {
-	 @FXML private ListView<Grade> quizListView;
-	 @FXML private ListView<Grade> homeworkListView;
-	 @FXML private ListView<Grade> examListView;
+	 @FXML private ListView<SimpleGrade> quizListView;
+	 @FXML private ListView<SimpleGrade> homeworkListView;
+	 @FXML private ListView<SimpleGrade> examListView;
 	 @FXML private TextField quizSubtotal;
 	 @FXML private TextField homeworkSubtotal;
 	 @FXML private TextField examSubtotal;
-	 private ObservableList<Grade> observableList;
+	 private ObservableList<SimpleGrade> observableList;
 
 	
+	 
 	 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
-		quizListView = new ListView<>();
-		
-		observableList = FXCollections.observableArrayList();
+		quizListView = new ListView<SimpleGrade>();
+		observableList = FXCollections.observableArrayList(new SimpleGrade(10));
+		quizListView.setItems(observableList);
 		quizListView.setEditable(true);
-        quizListView.setItems(observableList);
-     
-        quizListView.setCellFactory(lv -> new ListCell<Grade>() {
-            
-        	private TextField textField = new TextField() ;
-
-            {
-                textField.setOnAction(e -> {
-                    commitEdit(getItem());
-                   
-                });
-                textField.addEventFilter(KeyEvent.KEY_RELEASED, e -> {
-                    if (e.getCode() == KeyCode.ESCAPE) {
-                        cancelEdit();
-                    }
-                });
-            }
-       
+	     
 	
-             @Override
-             protected void updateItem(Grade grade, boolean empty) {
-                    super.updateItem(grade, empty);
-                    if (empty) {
-                        setText(null);
-                        setGraphic(null);
-                    } else if (isEditing()) {
-                        textField.setText(String.format("%.2f", grade.getValue()));
-                        setText(null);
-                        setGraphic(textField);
-                    } else {
-                        setText(String.format("%.2f", grade.getValue()));
-                        setGraphic(null);
-                    }
-                }
-           @Override
-             public void startEdit() {
-                 super.startEdit();
-                 textField.setText(String.format("%.2f", getItem().getValue()));
-                 setText(null);
-                 setGraphic(textField);
-                 textField.selectAll();
-                 textField.requestFocus();
-             }
+		quizListView.setCellFactory(TextFieldListCell.forListView(new StringConverter<SimpleGrade>(){
 
-            
+			@Override
+            public String toString(SimpleGrade simpleGrade) {
+                return simpleGrade.toString();
+            }
 
-             @Override
-             public void commitEdit(Grade grade) {
-                 super.commitEdit(grade);
-                 observableList.add(grade);
-                 grade.getValue();
-                 setText(String.format("%.2f", getItem().getValue()));
-                 setGraphic(null);
-                 
-             }
-             
-             @Override
-             public void cancelEdit() {
-                 super.cancelEdit();
-                 setText(String.format("%.2f", getItem().getValue()));
-                 setGraphic(null);
-             }
-         });
+            @Override
+            public SimpleGrade fromString(String string) {
+            	
+                return new SimpleGrade(Double.valueOf(string));
+            }
+
+	    }));
+
+          
 	}
-
-        
 	@FXML
 	public void clickRecalculate(ActionEvent event) {
 		CompositeGradeSum sumQuiz = new CompositeGradeSum();
@@ -123,7 +75,7 @@ public class SchoolGradesController implements Initializable {
 	
 	@FXML
 	public void addQuiz(ActionEvent event) {
-		//observableList.add
+		observableList.add(new SimpleGrade(10));
 		
 		//change to add user input
 		
