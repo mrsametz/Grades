75037268665b806b92cd5115a37901949accbbf5
Ashley Metz <mrsas@192.168.0.5>
2019-12-03 13:14:31 -0700

    Updated formatting

--- src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeGetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeGetValue.java
@@ -10,25 +10,25 @@ public class TestWeightedGradeGetValue {
 	private static final double DELTA = 0.001;
 	private WeightedGrade weightedGrade;
 	private Grade mockGrade;
-	
+
 	@BeforeEach
 	public void setup() {
 		mockGrade = mock(Grade.class);
 		when(mockGrade.getValue()).thenReturn(100.00);
 	}
-	
+
 	@Test
 	public void shouldApplyZeroWeight() {
 		weightedGrade = new WeightedGrade(mockGrade, 0);
 		assertEquals(0, weightedGrade.getValue(), DELTA);
 	}
-	
+
 	@Test
 	public void shouldApplyWeightOfOne() {
 		weightedGrade = new WeightedGrade(mockGrade, 1);
 		assertEquals(100, weightedGrade.getValue(), DELTA);
 	}
-	
+
 	@Test
 	public void shouldApplyWeightBetweenZeroAndOne() {
 		weightedGrade = new WeightedGrade(mockGrade, 0.5);
