7cd7ad566082d54ab0dcbe187f88f232c05ed4ef
Ashley Metz <mrsas@192.168.0.5>
2019-12-03 13:11:39 -0700

    Changed formatting

--- src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeConstructor.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeConstructor.java
@@ -1,7 +1,6 @@
 package edu.westga.cs.schoolgrades.model;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
@@ -9,21 +8,21 @@ public class TestWeightedGradeConstructor {
 
 	@Test
 	public void shouldNotAllowNullGrade() {
-		assertThrows(IllegalArgumentException.class, () ->{ 
+		assertThrows(IllegalArgumentException.class, () -> {
 			new WeightedGrade(null, 10.0);
 		});
 	}
-	
+
 	@Test
 	public void shouldNotAllowNegativeWeight() {
-		assertThrows(IllegalArgumentException.class, () ->{ 
+		assertThrows(IllegalArgumentException.class, () -> {
 			new WeightedGrade(mock(Grade.class), -10.0);
 		});
 	}
-	
+
 	@Test
 	public void shouldNotAllowWeightGreaterThanOne() {
-		assertThrows(IllegalArgumentException.class, () ->{ 
+		assertThrows(IllegalArgumentException.class, () -> {
 			new WeightedGrade(mock(Grade.class), 10.0);
 		});
 	}
