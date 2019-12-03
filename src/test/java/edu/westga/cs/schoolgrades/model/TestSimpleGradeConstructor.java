7f4608831fb513cdc80ec6d617d757e01c1ff8ae
Ashley Metz <mrsas@192.168.0.5>
2019-12-03 14:01:03 -0700

    Formatting

--- src/test/java/edu/westga/cs/schoolgrades/model/TestSimpleGradeConstructor.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestSimpleGradeConstructor.java
@@ -1,7 +1,7 @@
 package edu.westga.cs.schoolgrades.model;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 
 public class TestSimpleGradeConstructor {
